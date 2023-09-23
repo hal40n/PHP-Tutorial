dd1328eb3fdf4cc4a340ad23f302a27906db4957
hal40n <8140ndayz@gmail.com>
2023-09-23 11:33:05 +0900

    add laravel. composer and so on.

--- mente/db_connection.php
+++ mente/db_connection.php
@@ -1,6 +1,6 @@
 <?php
 
-  const DB_HOST = 'mysql:dbname=udemy_php;host=127.0.0.1;charset=utf8';
+  const DB_HOST = 'mysql:dbname=udemy_php;host=127.0.0.1ls;charset=utf8';
   const DB_USER = 'php_user';
   const DB_PASSWORD = 'password123';
 
